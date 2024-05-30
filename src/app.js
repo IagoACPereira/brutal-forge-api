245ddd779f3d89c964446454aa42e614c592c4a7
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-05-30 01:34:42 -0300

    create: views

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const gravadoraRouter = require('./routes/GravadoraRoutes');
 const artistaRouter = require('./routes/ArtistaRoutes');
 const albumRouter = require('./routes/AlbumRoutes');
 const faixaRouter = require('./routes/FaixaRoutes');
+const Album = require('./models/Album');
 
 const app = express();
 
@@ -17,7 +18,16 @@ app
   )
   .set('view engine', 'ejs')
   .set('views', `${__dirname}/views`)
-  .get('/', (req, res) => res.status(200).render('index.ejs'))
+  .get('/', async (req, res) => {
+    try {
+      res.status(200).render('index.ejs')
+    } catch (error) {
+      res.status(400).json({
+        mensagem: error.messege,
+        status: 400,
+      });
+    }
+  })
   .use(
     paisRouter,
     generoRouter,
