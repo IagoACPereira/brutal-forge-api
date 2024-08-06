dadeec3e415b777e8f4ae1add1e754a486c1fcef
Iago Azevedo Costa Pereira <94400482+IagoACPereira@users.noreply.github.com>
2024-08-06 13:41:08 +0000

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-require('dotenv').config();
-const app = require('./api/app');
+import 'dotenv/config';
 
 const porta = process.env.PORTA;
-
-// eslint-disable-next-line no-console
-app.listen(porta, () => console.log(`Servidor ok | http://localhost:${porta}`));
