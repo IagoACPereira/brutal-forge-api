32c72bf67a817b0f826411a42d8c88f58ea06f34
Iago Azevedo Costa Pereira <94400482+IagoACPereira@users.noreply.github.com>
2024-08-06 13:14:10 +0000

    feat: new first commit

--- api/config/conexaoDb.js
+++ api/config/conexaoDb.js
@@ -1,13 +1,18 @@
 const { Sequelize } = require('sequelize');
 
+// const sequelize = new Sequelize({
+//   host: process.env.HOSTDB,
+//   port: process.env.PORTADB,
+//   database: process.env.DATABASE,
+//   username: process.env.USERDB,
+//   password: process.env.PASSDB,
+//   dialect: process.env.DIALECT,
+// });
+
 const sequelize = new Sequelize({
-  host: process.env.HOSTDB,
-  port: process.env.PORTADB,
-  database: process.env.DATABASE,
-  username: process.env.USERDB,
-  password: process.env.PASSDB,
-  dialect: process.env.DIALECT,
-});
+  storage: './api/config/forja-bruta.sqlite',
+  dialect: 'sqlite'
+})
 
 try {
   sequelize.authenticate();
